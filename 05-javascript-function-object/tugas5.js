59f30d7b4386df38c6ee1e9ff3b13b7c4dfa7e7c
Ammar Jawas <jawasammar@gmail.com>
2024-03-30 01:27:16 +0700

    Tugas 5

---
+++ 05-javascript-function-object/tugas5.js
@@ -0,0 +1,109 @@
+// Soal 1
+// function cetakFunction() {
+//   return "Halo nama saya Ammar";
+// }
+
+// console.log(cetakFunction());
+
+// Soal 2
+// function myFunction(a, b) {
+//   return a + b;
+// }
+// let angka1 = 20;
+
+// let angka2 = 7;
+
+// let output = myFunction(angka1, angka2);
+
+// console.log(output);
+
+// Soal 3
+// const Hello = () => {
+//   return "Hello";
+// };
+
+// console.log(Hello());
+
+// Soal 4
+// let obj = {
+//   nama: "john",
+
+//   umur: 22,
+
+//   bahasa: "indonesia",
+// };
+
+// console.log(obj.bahasa);
+
+// Soal 5
+// let arrayDaftarPeserta = ["John Doe", "laki-laki", "baca buku", 1992];
+
+// let objDaftarPeserta = {
+//   nama: arrayDaftarPeserta[0],
+//   gender: arrayDaftarPeserta[1],
+//   hobby: arrayDaftarPeserta[2],
+//   tahunLahir: arrayDaftarPeserta[3],
+// };
+// console.log(objDaftarPeserta);
+
+// Soal 6
+// let buah = [
+//   {
+//     nama: "Nanas",
+//     warna: "Kuning",
+//     adaBijinya: false,
+//     harga: 9000,
+//   },
+//   { nama: "Jeruk", warna: "Oranye", adaBijinya: true, harga: 8000 },
+//   { nama: "Semangka", warna: "Hijau & Merah", adaBijinya: true, harga: 10000 },
+//   { nama: "Pisang", warna: "Kuning", adaBijinya: false, harga: 5000 },
+// ];
+
+// let result = buah.filter(function (el) {
+//   return el.adaBijinya == false;
+// });
+
+// console.log(result);
+
+// Soal 7
+// let phone = {
+//   name: "Galaxy Fold 5",
+//   brand: "Samsung",
+//   year: 2023,
+// };
+// // kode diatas ini jangan di rubah atau di hapus sama sekali
+// const { name, brand, year } = phone;
+// // kode di bawah ini jangan dirubah atau dihapus
+// console.log(name, brand, year);
+
+// Soal 8
+// let dataBukuTambahan = {
+//   penulis: "john doe",
+//   tahunTerbit: 2020,
+// };
+
+// let buku = {
+//   nama: "pemograman dasar",
+//   jumlahHalaman: 172,
+// };
+
+// let objOutput = { ...dataBukuTambahan, ...buku };
+
+// // kode diatas ini jangan di rubah atau di hapus sama sekali
+
+// // kode di bawah ini jangan dirubah atau dihapus
+// console.log(objOutput);
+
+// Soal 9
+// let mobil = {
+//   merk: "bmw",
+
+//   color: "red",
+
+//   year: 2002,
+// };
+
+// const functionObject = (param) => {
+//   return param;
+// };
+// console.log(functionObject(mobil));
