f71090ad3521b2874e49748c70865d00b9c968db
Ammar Jawas <jawasammar@gmail.com>
2024-04-02 23:12:43 +0700

    Tugas 6 React

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="h1">Hi, Saya Ammar</h1>
+        <hr className="hr"></hr>
+        <p className="p">
+          Saya dari batch 55 dan sudah berhasil menginstall react
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
